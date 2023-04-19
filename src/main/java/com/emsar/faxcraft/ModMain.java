f7860e83598214971f9d40d1ef2a58d4f5fb2571
BuildTools <cemilcansiz.cc@gmail.com>
2023-04-19 21:21:28 +0300

    First Commit

--- src/main/java/com/emsar/faxcraft/ModMain.java
+++ src/main/java/com/emsar/faxcraft/ModMain.java
@@ -1,8 +1,13 @@
 package com.emsar.faxcraft;
 
+import com.emsar.faxcraft.util.ItemRegister;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 @Mod(ModMain.MODID)
@@ -10,10 +15,21 @@ public class ModMain
 {
     public static final String MODID = "faxcraft";
 
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final CreativeModeTab TAB = new CreativeModeTab("main") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ItemRegister.bor.get());
+        }
+    };
 
     public ModMain()
     {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ItemRegister.registerItems(bus);
+
         MinecraftForge.EVENT_BUS.register(this);
     }
 }
---
+++ src/main/java/com/emsar/faxcraft/util/ItemRegister.java
@@ -0,0 +1,21 @@
+package com.emsar.faxcraft.util;
+
+import com.emsar.faxcraft.ModMain;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ItemRegister {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ModMain.MODID);
+
+    public static final RegistryObject<Item> bor = ITEMS.register("bor", () ->
+            new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD))
+    );
+
+    public static void registerItems(IEventBus bus){
+        ITEMS.register(bus);
+    }
+}
